7a24d0a157458e4ced4d337752626513300f1087
HaithamDesouky <haithamadesouky@gmail.com>
2020-09-06 19:02:36 +0100

    mystery section started

--- client/src/views/Home/HomeView.jsx
+++ client/src/views/Home/HomeView.jsx
@@ -6,6 +6,10 @@ import { render } from 'react-dom';
 import ItemsCarousel from '../../components/ItemsCarousel';
 import coins from './treasurechest.png';
 import './HowItWorksSection.scss';
+import './MysteryBox.scss';
+
+import lootboxes from './lootboxes.png';
+import mysterybox from './mystery.png';
 
 // import { listProducts } from '../services/product';
 
@@ -49,7 +53,7 @@ class HomeView extends Component {
             <p>
               Here in Lucky Loot Box, our main currency are credits! You can buy
               those with real money using a credit or debit card or by
-              participating in our community!{' '}
+              participating in our community!
             </p>
             <Link to="/credits">Buy Credits Now!</Link>
           </div>
@@ -61,18 +65,49 @@ class HomeView extends Component {
                   <span className="orange-tag">Step 2: </span> Choose your
                   lootbox!
                 </h2>
-                <h2>
-                  We have 3 types of boxes that container <strong>3</strong>{' '}
-                  items chosen at randomly. Every time you buy a Lucky LootBox
-                  you are{' '}
+                <p>
+                  We have 3 types of boxes that contain <strong>3</strong> items
+                  chosen at random, in the category of your choice. Every time
+                  you buy a Lucky LootBox you are{' '}
                   <span className="orange-tag">
                     guaranteed not to get the same items again!
                   </span>
-                </h2>
+                </p>
+
+                <h3>The 3 catergories are: </h3>
+                <ul>
+                  <li>
+                    <i class="fas fa-tv"></i> Anime
+                  </li>
+                  <li>
+                    <i class="fas fa-gamepad"></i> Video Games
+                  </li>
+                  <li>
+                    <i class="fas fa-film"></i> Entertainment{' '}
+                  </li>
+                </ul>
               </div>
-              <div>second part</div>
             </div>
           </div>
+          <div className="vertical-boxes">
+            <img src={lootboxes} alt="" />
+          </div>
+        </section>
+
+        <section className="mystery-box">
+          <h1>The Mystery Box</h1>
+          <img src={mysterybox} alt="" />
+          <p>
+            For just <span className="orange-tag">75 credits</span> extra you
+            can try your hand at the{' '}
+            <span className="orange-tag">MYSTERY BOX!</span>
+            <p>
+              The Mystery Box contains <span className="orange-tag">FIVE</span>{' '}
+              items! Yup, not three but{' '}
+              <span className="orange-tag">FIVE!</span> However, the categories
+              are chosen at random! It's up to you to take on the risk!
+            </p>
+          </p>
         </section>
 
         <div className="second carousel">
