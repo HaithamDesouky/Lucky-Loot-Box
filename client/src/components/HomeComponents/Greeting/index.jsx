142599613d6c5398be1c56f8f27f2e9eb8a51e17
HaithamDesouky <haithamadesouky@gmail.com>
2020-09-09 00:23:53 +0100

    animations added

--- client/src/components/HomeComponents/Greeting/index.jsx
+++ client/src/components/HomeComponents/Greeting/index.jsx
@@ -9,6 +9,7 @@ export default function index() {
       <h3>
         Highly valued boxes filled with curated items ranging from video games
         consoles, t-shirts, memorabilla and much more!
+        <br /> Scroll down to find out more!
       </h3>
 
       <Link to="/shop" className="home-links">
